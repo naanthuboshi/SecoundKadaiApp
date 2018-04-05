0b045bca923f45ebd723b0100f5148a3398cbff5
山口奈々 <7stars.0296@gmail.com>
2018-04-05 21:49:40 +0900

    再提出ようです。

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -2,24 +2,21 @@
 //  ResultViewController.swift
 //  SecondKadaiApp
 //
-//  Created by Mac Owner on 2018/03/26.
+//  Created by Mac Owner on 2018/04/03.
 //  Copyright © 2018年 Mac Owner. All rights reserved.
 //
 
 import UIKit
 
 class ResultViewController: UIViewController {
-    var name = ""
-    @IBOutlet var yourname: UILabel!
-    
-    @IBAction func hellobutton(_ sender: Any) {
-    }
+    @IBOutlet weak var hello: UILabel!
+    var yourname = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-     yourname.text = name
+hello.text = "こんにちは、\(yourname)さん"
         // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  SecondKadaiApp
 //
-//  Created by Mac Owner on 2018/03/15.
+//  Created by Mac Owner on 2018/04/03.
 //  Copyright © 2018年 Mac Owner. All rights reserved.
 //
 
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yourname: UIButton!
+
+    @IBOutlet weak var nametextfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // segueから遷移先のResultViewControllerを取得する
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
         // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
-        resultViewController.name = ""
+        resultViewController.yourname = nametextfield.text!
         
+     
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -27,5 +32,14 @@ class ViewController: UIViewController {
     }
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
-}
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
